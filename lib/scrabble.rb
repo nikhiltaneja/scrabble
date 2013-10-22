ad0ec05852bdd92b1b02e0511c01d416818d742f
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-10-22 14:46:38 -0600

    Added highest scoring method

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,5 +1,43 @@
 class Scrabble
   def self.score(word)
-    0
+    return 0 if word.nil? || word.empty?   
+    score_letters(word.chars).inject(:+)
+  end
+
+  def self.score_letters(letters)
+    letters.collect do |letter|
+      score_letter(letter)
+    end
+  end
+
+  def self.score_letter(letter)
+    score_card[letter.upcase]
+  end
+
+  def self.score_card
+    {
+      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+      "Y"=>4, "Z"=>10
+    }
+  end
+
+  def self.highest_score_from(words)
+    #create a variable to hold the highest score
+    highest_score = 0
+    #create a variable to hold the corresponding word
+    high_word = ""
+    words.each do |word|
+      if score(word) > highest_score
+        highest_score = score(word)
+        high_word = word
+      end
+    end
+    high_word
+
   end
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -8,4 +8,20 @@ class ScrabbleTest < Minitest::Test
     assert_equal 1, Scrabble.score("a")
     assert_equal 4, Scrabble.score("f")
   end
+
+  def test_nil_is_scored_as_zero
+    assert_equal 0, Scrabble.score(nil)
+  end
+
+  def test_empty_is_scored_as_zero
+    assert_equal 0, Scrabble.score("")
+  end
+
+  def test_it_scores_a_word
+    assert_equal 8, Scrabble.score("hello")
+  end
+
+  def test_it_selects_the_highest_scoring_word_from_group
+    assert_equal "home", Scrabble.highest_score_from(['home', 'word', 'hello', 'sound'])
+  end
 end
